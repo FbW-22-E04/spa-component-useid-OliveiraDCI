5ccb98cbc1a15b7954b222fff794f667f2818b7d
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-11-02 15:08:35 +0100

    Exercise concluded.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useId } from "react";
+
 import "./App.css";
 
 function App() {
+  const id1 = useId();
+  const id2 = useId();
+  const id3 = useId();
   return (
     <div className="App">
-        
+      <form className="form" action="submit">
+        <label htmlFor={id1}>
+          First Name <input type="text" name="first-name" id={id1} />
+        </label>
+        <label htmlFor={id2}>
+          Last Name <input type="text" name="last-name" id={id2} />
+        </label>
+        <label htmlFor={id3}>
+          Email <input type="email" name="email" id={id3} />
+        </label>
+      </form>
     </div>
   );
 }
